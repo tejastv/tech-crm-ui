bb4aa4a9c3ec6c45c41b4c128a8ae084bd85a297
Piyush <Piyush24@gmail.com>
2023-09-30 20:32:24 +0530

    Transaction Master complete

--- src/pages/transaction-search/index.ts
+++ src/pages/transaction-search/index.ts
@@ -6,7 +6,16 @@ export { Enquiries } from "./all-enquiries/AllEnquiries";
 
 export { EnquirySearch } from "./enquiry-search/EnquirySearch";
 
+export { PurchaseBills } from "./purchase-bills/PurchaseBills";
+export { AddUpdatePurchase } from "./purchase-bills/AddUpdatePurchaseBills";
+export { addPurchaseBillsFormFields } from "./purchase-bills/add-purchase-bills-form-field";
+
+export { AddBulkEnquiries } from "./add-bulk-enquiries/AddBulkEnquiries";
+
 export { addEnquirySearchFormFields } from "./enquiry-search/add-enquiry-search-form-field";
+export { addBulkEnquiriesFormFields } from "./add-bulk-enquiries/add-bulk-enquires-form-field";
+
+
 export * from "./add-new-enquiry/add-update-new-emquiry-type";
 export * from "./add-new-enquiry/new-enquiry-type";
 
