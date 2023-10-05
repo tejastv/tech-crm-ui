af54996de5d3534835611a6e2c312204c87317e2
Piyush <Piyush24@gmail.com>
2023-10-05 15:55:27 +0530

    Task MCRM-69 Complete

--- src/pages/profoma/index.ts
+++ src/pages/profoma/index.ts
@@ -3,8 +3,15 @@ export { AddGeneratePi } from "./generate-pi/AddGeneratePi";
 export { SearchGeneratePi } from "./generate-pi/SearchGeneratePi";
 
 
+export { EnqPi } from "./enq-pi/EnqPi";
+export { AddEnqPi } from "./enq-pi/AddEnqPi";
+export { SearchEnqPi } from "./enq-pi/SearchEnqPi";
+
+
 
 export { addGeneratePiFormFields } from "./generate-pi/add-generate-pi-form-field";
+
+export { addEnqPiFormFields } from "./enq-pi/add-enq-pi-form-field";
 
 
 
